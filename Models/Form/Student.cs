b141b2b7a0fb0cd1006ac10fa8fb1a9ebbba9682
Miguel Angelo D. Ildefonso <migs.ildefonso@JPI.Local>
2021-05-07 15:00:36 +0800

    Added add student on admin page

--- Models/Form/Student.cs
+++ Models/Form/Student.cs
@@ -1,35 +1,24 @@
 using System;
-using LinqToDB.Mapping;
-using System.ComponentModel.DataAnnotations;
-
-namespace SchoolProject.Models.Data
+namespace SchoolProject.Models.Form
 {
-    [Table]
     public class Student
     {
-        [Key]
         public Int32 student_id{get; set;}
-        [Column]
         public string firstname{get; set;}
-        [Column]
         public string middlename{get; set;}
-        [Column]
         public string lastname{get; set;}
-        [Column]
         public DateTime birthday{get; set;}
-        [Column]
         public string program{get; set;}
-        [Column]
         public string status{get; set;}
-        [Column]
         public Int32 sem{get; set;}
-        [Column]
         public Int32 yearlevel {get; set;}
-        [Column]
         public Int32 academic_year{get; set;}
-        [Column]
         public string username{get; set;}
-        [Column]
         public string password{get; set;}
+    }
+
+    public enum StudStatus
+    {        
+        Regular, Irregular
     }
 }
